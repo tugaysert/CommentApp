0d4dd533fa9276064e04dd55d16c58fd7b04df65
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-04-01 20:07:10 +0300

    first commit

---
+++ commentapp/src/main/java/com/example/commentapp/CommentappApplication.java
@@ -0,0 +1,13 @@
+package com.example.commentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentappApplication.class, args);
+	}
+
+}
---
+++ commentapp/src/test/java/com/example/commentapp/CommentappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
